216894f6e3ba8a587ecf761fdbbbaa549ed9ebac
danielriley06 <driley3@gmail.com>
2016-08-12 18:18:51 -0500

    fix accounts controller and added index view and blank datatable controller for accounts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,18 +1,22 @@
 class AccountsController < ApplicationController
-  before_action :set_brewery, only: [:show]
+  #before_action :set_brewery, only: [:index]
+  before_action :authenticate_user!
 
-  def show
+  # GET /breweries
+  # GET /breweries.json
+  def index
+    @user = current_user
     respond_to do |format|
       format.html
-      format.json { render json: ReviewDatatable.new(view_context) }
+      format.json { render json: AccountDatatable.new(view_context) }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_brewery
-      @brewery = Brewery.find(params[:id])
-    end
+    #def set_brewery
+    #  @brewery = Brewery.find(params[:id])
+    #end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def brewery_params
