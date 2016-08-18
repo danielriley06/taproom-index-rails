250171d2035a7dc51537e41a345a2e3138174cc2
danielriley06 <driley3@gmail.com>
2016-08-18 01:09:15 -0400

    updated login form to match new styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   has_many :reviews
   has_many :breweries, through: :reviews
 
+  attr_accessor :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
