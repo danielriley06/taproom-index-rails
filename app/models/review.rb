00d983294e4b6cb77945213ffe3f7f7e66673c8b
danielriley06 <driley3@gmail.com>
2016-08-25 16:38:04 -0400

    updated review form to handle range of 1 to 10 for ratings

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,7 @@ class Review < ActiveRecord::Base
   belongs_to :brewery
 
   validates :ambiance, :service, :selection, :food, :value, :date_visited, presence: true
-  validates :ambiance, :service, :selection, :food, :value,  :inclusion => { :in => 0..20 }
+  validates :ambiance, :service, :selection, :food, :value,  :inclusion => { :in => 1..10 }
 
 
 end
