f82767b752aa4df59dd0fe3274c77231945391e8
danielriley06 <driley3@gmail.com>
2016-09-01 15:08:13 -0400

    asset fixes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.1'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w(datatables.js landing.css landing.js)
+Rails.application.config.assets.precompile += %w(datatables.js landing.css landing.js markerclusterer.js)
