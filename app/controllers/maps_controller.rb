7c983d8b72e1bcf0bced8ccfdf4e1d656fa803a9
danielriley06 <driley3@gmail.com>
2016-09-01 19:33:51 -0400

    infowindow update to add info

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -3,7 +3,7 @@ class MapsController < AccountsController
   def brewmap
     @breweries = Brewery.search(params[:search])
     @brewmarkers = @breweries.map do |b|
-       [ b.name, b.latitude, b.longitude ]
+       [ b.latitude, b.longitude, b.name, b.street_address, b.telephone, b.brewery_url ]
     end
     @markers = @brewmarkers.to_json
   end
