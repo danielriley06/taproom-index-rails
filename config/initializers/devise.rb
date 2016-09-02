0c38bd9a56a64720aea5ab69edd7574f0abd769c
danielriley06 <driley3@gmail.com>
2016-09-01 20:07:55 -0400

    updated oauth redirect_uri

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -247,7 +247,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :google_oauth2, "397468786914-arqb0hml02aes1oofcg223kol4mcg50u.apps.googleusercontent.com", "yn0E4DzU0RvPkyBxEgHCICd3", :redirect_uri => 'http:brewpub.guru/users/auth/google_oauth2/callback'
+  config.omniauth :google_oauth2, "397468786914-arqb0hml02aes1oofcg223kol4mcg50u.apps.googleusercontent.com", "yn0E4DzU0RvPkyBxEgHCICd3", :redirect_uri => 'http://brewpub.guru/users/auth/google_oauth2/callback'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
